fe30d94506b50c9db5fd77c7c169490a74c8fa4c
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-12-09 16:37:25 +1100

    Create variable_format.cs

---
+++ variable_format.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Number of pizza shops used int - whole number
+      int pizzaShops = 4332;
+
+
+      // Number of employees - used int - whole number
+      int totalEmployees = 86928;
+
+
+      // Revenue - used decimal recommended for finance, with letter m after the number
+      decimal revenue = 390819.28m;
+
+
+
+      // Log the values to the console:
+      Console.WriteLine(pizzaShops);
+      Console.WriteLine(totalEmployees);
+      Console.WriteLine(revenue);
+
+   	}
+  }
+}
